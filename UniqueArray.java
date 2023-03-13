2dd83d9a0a81b3259f52cab487af85085940bc89
nullr4pt0r <67836886+johnthebug@users.noreply.github.com>
2023-03-13 22:12:36 +0530

    Update UniqueArray.java

--- UniqueArray.java
+++ UniqueArray.java
@@ -1,10 +1,12 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class UniqueArray {
 
     static void removeDuplicates(int[] numbers, int length){
         Arrays.sort(numbers); // sorting array
-
+        long start = System.nanoTime();
         int j=0;
         for(int i=0;i<length-1;i++){
             if(numbers[i] != numbers[i+1]){
@@ -12,11 +14,12 @@ public class UniqueArray {
             }
         }
         numbers[j++] = numbers[length-1];
+        long end = System.nanoTime();
         System.out.println("Using Constant space ...");
         for(int i=0;i<j;i++){
             System.out.print(numbers[i]+" ");
         }
-
+        System.out.println("\n Time taken to Remove Duplicate : "+ (end - start)+" nano seconds ");
     }
 
     static void sort(int[] array,int length){
@@ -24,9 +27,9 @@ public class UniqueArray {
         for(int i=0;i< length;i++){
             int minIndex = i;
             for(int j=i+1;j< length;j++){
-            if(array[j] < array[minIndex]){
+                if(array[j] < array[minIndex]){
                     minIndex = j;
-            }
+                }
             }
             int temp = array[i];
             array[i] = array[minIndex];
@@ -39,7 +42,7 @@ public class UniqueArray {
         System.out.println("\n Time taken to sort : "+ (end - start)+" nano seconds ");
     }
 
-       static void removeDuplicatesUsingTempArray(int[] number,int length){
+    static void removeDuplicatesUsingTempArray(int[] number,int length){
         long start = System.nanoTime();
         int[] values = new int[number.length];
         int j=0;
@@ -54,9 +57,8 @@ public class UniqueArray {
         for(int i=0;i<j;i++){
             System.out.print(values[i]+" ");
         }
-       System.out.println("\n Time taken to sort : "+ (end - start)+" nano seconds ");
+        System.out.println("\n Time taken to remove duplicate : "+ (end - start)+" nano seconds ");
     }
-
 
     public static void main(String[] args) {
         int[] numbers1 = { 51, 22, 69, 57, 46, 77, 85, 9, 33, 48, 2, 99, 88, 73, 41, 68, 5, 30, 92, 13 }; //unsorted array
@@ -65,11 +67,5 @@ public class UniqueArray {
         sort(numbers,length);
         removeDuplicatesUsingTempArray(numbers,length);
         removeDuplicates(numbers,length); //using constant space
-
-
-
-
     }
-
-
 }

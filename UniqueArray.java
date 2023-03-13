1c8b870d62be711527f030353de12df52b952cdc
nullr4pt0r <67836886+johnthebug@users.noreply.github.com>
2023-03-13 22:10:07 +0530

    Update UniqueArray.java

--- UniqueArray.java
+++ UniqueArray.java
@@ -37,9 +37,26 @@ public class UniqueArray {
         long end = System.nanoTime();
         for(int num : array){
             System.out.print(num+" ");
-
         }
         System.out.println("\n Time taken to sort : "+ (end - start)+" nano seconds ");
+    }
+
+       static void removeDuplicatesUsingTempArray(int[] number,int length){
+        long start = System.nanoTime();
+        int[] values = new int[number.length];
+        int j=0;
+        for(int i=0;i<length-1;i++){
+            if(number[i] != number[i+1]){
+                values[j++] = number[i];
+            }
+        }
+        values[j++] = number[length - 1];
+        long end = System.nanoTime();
+        System.out.println("\n Using Auxillary space ...");
+        for(int i=0;i<j;i++){
+            System.out.print(values[i]+" ");
+        }
+       System.out.println("\n Time taken to sort : "+ (end - start)+" nano seconds ");
     }
 
 
@@ -48,6 +65,7 @@ public class UniqueArray {
         int[] numbers = {1, 2, 2, 4, 5, 5, 5, 7, 7, 8, 9, 9, 9, 11, 11, 11, 11, 13, 15, 15};
         int length = numbers.length;
         sort(numbers,length);
+        removeDuplicatesUsingTempArray(numbers,length);
         removeDuplicates(numbers,length); //using constant space
 
 
